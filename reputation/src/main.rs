c31e773f1de5fd27532bcca1bd8f5aa0bdcb04f8
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2023-08-28 10:54:18 +0200

    initial commit

---
+++ reputation/src/main.rs
@@ -0,0 +1,72 @@
+extern crate curve25519_dalek;
+extern crate rand;
+
+use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
+use curve25519_dalek::scalar::Scalar;
+use rand::Rng;
+
+// Simulated reputation data
+struct ReputationData {
+    positive_vouches: u32,
+    negative_vouches: u32,
+    // Other reputation attributes...
+}
+
+// Simulated user
+struct User {
+    reputation_data: ReputationData,
+    secret_key: Scalar,
+}
+
+fn main() {
+    // Simulate Alice and Bob
+    let alice_secret_key = Scalar::random(&mut rand::thread_rng());
+    let alice_reputation_data = ReputationData {
+        positive_vouches: 10,
+        negative_vouches: 2,
+    };
+    let alice = User {
+        reputation_data: alice_reputation_data,
+        secret_key: alice_secret_key,
+    };
+
+    let bob_secret_key = Scalar::random(&mut rand::thread_rng());
+    let bob_reputation_data = ReputationData {
+        positive_vouches: 8,
+        negative_vouches: 1,
+    };
+    let bob = User {
+        reputation_data: bob_reputation_data,
+        secret_key: bob_secret_key,
+    };
+
+    // Simulate proving reputation data
+    let mut rng = rand::thread_rng();
+    let challenge = Scalar::random(&mut rng);
+
+    // Alice generates commitment
+    let commitment_a = RISTRETTO_BASEPOINT_POINT * alice.secret_key;
+
+    // Alice computes response
+    let response_a = alice.secret_key + challenge * alice.reputation_data.positive_vouches;
+
+    // Alice sends commitment to Bob
+
+    // Bob generates commitment
+    let commitment_b = RISTRETTO_BASEPOINT_POINT * bob.secret_key;
+
+    // Bob computes response
+    let response_b = bob.secret_key + challenge * bob.reputation_data.positive_vouches;
+
+    // Bob sends commitment and response to Alice
+
+    // Alice verifies Bob's commitment and response
+    let expected_commitment_b = RISTRETTO_BASEPOINT_POINT * bob.secret_key;
+    let expected_response_b = bob.secret_key + challenge * bob.reputation_data.positive_vouches;
+
+    if commitment_b == expected_commitment_b && response_b == expected_response_b {
+        println!("Bob's reputation proof is valid.");
+    } else {
+        println!("Bob's reputation proof is invalid.");
+    }
+}
